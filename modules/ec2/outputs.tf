142846bf98f1c0c64aacbde274e459e825c2dc2d
José Guilherme Pinto Linhares <jose.linhares@tjce.jus.br>
2022-11-26 13:05:24 -0300

    add public_ip in outputs message

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,9 @@
 output "instance_id" {
+  description = "ID of instances"
   value       = aws_instance.aws_ec2.*.id
+}
+
+output "instances_public_ip" {
+  description = "Ip of instances"
+  value       = aws_instance.aws_ec2.*.public_ip
 }
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "instance_id" {
   value       = module.ec2_instance.*.instance_id
 }
 
+output "instances_public_ip" {
+  value = module.ec2_instance.*.instances_public_ip
+}
+
 output "rds_id" {
   value = ""
 }
